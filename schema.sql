6dbee3de9c63c8c10919c1643947fde39af4eec5
dldnfla <lwr0221716@naver.com>
2023-11-10 16:33:27 +0900

    fix error

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "USERS"(
+CREATE TABLE IF NOT EXISTS "USERS"(
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
     "username" TEXT,
@@ -6,15 +6,15 @@ CREATE TABLE "USERS"(
     "state_comment" TEXT
 );
 
-CREATE TABLE "FOLLOW_REQUEST"(
+CREATE TABLE IF NOT EXISTS "FOLLOW_REQUEST"(
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "username" TEXT NOT NULL,
     "target_username" TEXT NOT NULL,
-    "following_state" INTEGER NOT NULL,
+    "following_state" INTEGER NOT NULL
 );
 
-CREATE TABLE "MATE_RELATIONSHIP"(
+CREATE TABLE IF NOT EXISTS "MATE_RELATIONSHIP"(
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "first_username" TEXT NOT NULL,
-    "second_username" TEXT NOT NULL,   
-)
+    "second_username" TEXT NOT NULL 
+);
