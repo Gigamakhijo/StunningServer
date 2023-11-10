aa9fd96cd126da3473486892d608504ce346c5f3
dldnfla <lwr0221716@naver.com>
2023-11-10 15:32:04 +0900

    add signup api

---
+++ app/main.py
@@ -0,0 +1,161 @@
+from datetime import datetime, timedelta
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+import sqlite3
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+
+class User(BaseModel):
+    email: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+con = sqlite3.connect("test.db")
+con.row_factory = sqlite3.Row
+cur = con.cursor()
+
+
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated = "auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(plain_password):
+    return pwd_context.hash(plain_password)
+
+
+def get_user(user):
+    try:
+        if user is not None:
+            user = dict(user)
+            return UserInDB(**user)
+    except HTTPException:
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password")
+    
+
+def authenticate_user(USER, plain_password: str):
+    user = get_user(USER)
+    if not user:
+        return False
+    if not verify_password(plain_password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(id=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    cur.execute("SELECT email,hashed_password FROM Users WHERE email=?",(form_data.username,))
+    USER = cur.fetchone()
+    
+    user = authenticate_user(USER,form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/register")
+async def register_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    cur.execute("SELECT email FROM Users WHERE email=?",(form_data.username,))
+    user = cur.fetchone()
+    if user is not None: 
+        raise HTTPException(status_code= status.HTTP_409_CONFLICT, detail= "User already exists")
+    
+    cur.execute("INSERT INTO Users(email,username,hashed_password,state_comment) VALUES(?,'',?,'')",
+                (form_data.username, get_password_hash(form_data.password),))
+    con.commit()
+    return HTTPException(status_code= status.HTTP_200_OK, detail= "Signup Successful")
+
+
+
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.email}]
